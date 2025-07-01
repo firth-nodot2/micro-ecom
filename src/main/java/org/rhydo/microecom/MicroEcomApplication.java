cfe6768d8adcf8aae455a37f9bade97cf542f096
firth-nodot2 <firthnodot2@gmail.com>
2025-07-01 08:51:40 +0800

    First Commit

---
+++ src/main/java/org/rhydo/microecom/MicroEcomApplication.java
@@ -0,0 +1,13 @@
+package org.rhydo.microecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroEcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rhydo/microecom/MicroEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rhydo.microecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroEcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
